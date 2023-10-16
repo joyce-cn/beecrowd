5975cf6a1895489a3eb30f1af3ff4e0e4cccadf6
joyce-cn <146238800+joyce-cn@users.noreply.github.com>
2023-10-16 15:22:31 -0300

    exercício_1052

---
+++ 1052.js
@@ -0,0 +1,26 @@
+let numero = lines.shift()
+if (numero == 1) {
+    console.log("January")
+}if (numero == 2) {
+    console.log("February")
+}if (numero == 3) {
+    console.log("March")
+}if (numero == 4) {
+    console.log("April")
+}if (numero == 5) {
+    console.log("May")
+}if (numero == 6) {
+    console.log("June")
+}if (numero == 7) {
+    console.log("July")
+}if (numero == 8) {
+    console.log("August")
+}if (numero == 9) {
+    console.log("September")
+}if (numero == 10) {
+    console.log("October")
+}if (numero == 11) {
+    console.log("November")
+}if (numero == 12) {
+    console.log("December")
+}
