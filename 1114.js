69fa8bf071d0887f09f48f7c16732b58087c1098
joyce-cn <146238800+joyce-cn@users.noreply.github.com>
2023-10-16 16:02:41 -0300

    exercício_1114

---
+++ 1114.js
@@ -0,0 +1,10 @@
+while(1){
+    let a = Number(lines.shift())
+      if(a === 2002){ 
+      console.log('Acesso Permitido')
+      break;
+      }else{
+      console.log('Senha Invalida')
+      }
+    }
+    
