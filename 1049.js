b4a2df54f344d78606f4de8a8f2e5170bbbb53f2
joyce-cn <146238800+joyce-cn@users.noreply.github.com>
2023-10-16 15:19:59 -0300

    exercício_1049

---
+++ 1049.js
@@ -0,0 +1,20 @@
+let a = (lines.shift())
+let b = (lines.shift())
+let c = (lines.shift())
+if (a == `vertebrado` && b == `ave` && c == `carnivoro`){ 
+console.log("aguia")
+}else if (a == "vertebrado" && b == "ave" && c == "onivoro"){
+console.log("pomba")
+}else if (a == `vertebrado` && b == `mamifero` && c == `onivoro`){
+console.log("homem")
+}else if (a == `vertebrado` && b == `mamifero` && c == `herbivoro`){
+console.log("vaca")
+} if (a == `invertebrado` && b == `inseto` && c == `hematofago`){
+console.log("pulga")
+}else if (a == `invertebrado` && b == `inseto` && c == `herbivoro`){
+console.log("lagarta")
+}else if (a == `invertebrado` && b == `anelideo` && c == `hematofago`){
+console.log("sanguessuga")
+}else if (a == `invertebrado` && b == `anelideo` && c == `onivoro`){
+console.log("minhoca")
+}
