f17f76d5ca66e469859f0afd357ff05a75853d89
joyce-cn <146238800+joyce-cn@users.noreply.github.com>
2023-10-16 16:23:20 -0300

    exercício_1142

---
+++ 1142.js
@@ -0,0 +1,11 @@
+let N = lines.shift()
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+
+for(let i = 0;i < N;i++){
+    console.log(`${numero1} ${numero2} ${numero3} PUM`)
+    numero1 += 4
+    numero2 += 4
+    numero3 += 4
+}
