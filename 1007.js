9d883a0979b59f5c722c16c8429ad00f23c329de
joyce-cn <146238800+joyce-cn@users.noreply.github.com>
2023-10-16 14:35:32 -0300

    exercício_1007

---
+++ 1007.js
@@ -0,0 +1,9 @@
+let a = Number(lines.shift())
+let b = Number(lines.shift())
+let c = Number(lines.shift())
+let d = Number(lines.shift())
+
+produto1 = a * b
+produto2 = c * d
+
+console.log("DIFERENCA =",produto1 - produto2)
